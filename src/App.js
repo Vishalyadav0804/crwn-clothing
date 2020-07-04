f61636f11554fb9772d219175c900b370ea8013c
Vishal Yadav <vishalyadav0804@gmail.com>
2020-07-04 21:52:19 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/'  component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+import './sign-in-and-sign-up.styles.css';
+
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+
+);
+
+export default SignInAndSignUpPage;
