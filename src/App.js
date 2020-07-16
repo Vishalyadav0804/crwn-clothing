4c2cfcdf9002abf9d04793dd782d92861c6afdea
Vishal Yadav <vishalyadav0804@gmail.com>
2020-07-16 17:39:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {createStructuredSelector} from 'reselect';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import CheckoutPage from './pages/checkout/checkout';
+
 import Header from './components/header/header';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
@@ -44,6 +46,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/'  component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route path='/checkout' component={CheckoutPage}/>
           <Route exact path='/signin' render={() =>
                                                 this.props.currentUser ? (
                                                   <Redirect to = '/' />
